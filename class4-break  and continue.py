91cc599e674905b4a673d492c2282d0a65e0af74
deepalisrivastva12 <115392693+deepalisrivastva12@users.noreply.github.com>
2022-12-10 22:40:51 +0530

    Add files via upload

---
+++ class4-break  and continue.py
@@ -0,0 +1,14 @@
+#break
+n= int(input())
+for i in range(0,n+1):
+     if i==4:
+         break
+     print(i)
+     
+
+#continue
+n= int(input())
+for i in range(0,n+1):
+    if i==4:
+        continue
+    print(i)

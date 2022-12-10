4b1291a86056bca9747fcb85607b95250bfdc3b7
deepalisrivastva12 <115392693+deepalisrivastva12@users.noreply.github.com>
2022-12-10 13:51:18 +0530

    Add files via upload

---
+++ class8-step argument-CipherSchools.py
@@ -0,0 +1,10 @@
+#print the no. from 1 to 20 with the gap 0f 3 numbers
+# for i in range(1,21,2):
+#      print(i)
+#      "/n"
+#print the no. from 20 to 1 wiht the gap f 3 number
+for i in range(21,-1,-3):
+
+    print(i)
+    "\n"
+print("dee")
